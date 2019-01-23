ac6f0b20aad9f8a6d8d7fbf446feba7801598481
fulgure <marco-k227@hotmail.com>
2019-01-23 13:57:54 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+    </head>
+    <body>
+        <?php
+        include "navbar.inc.php";
+        ?>
+    </body>
+</html>
---
+++ navbar.inc.php
@@ -0,0 +1,6 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="post.php">Post</a></li>
+    </ul>
+</nav>
