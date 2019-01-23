ea61503196632de8a915596fd363d077b24b6125
fulgure <marco-k227@hotmail.com>
2019-01-23 14:06:25 +0100

    Chapitre 1 terminé

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
-
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Home</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         <?php
         include "navbar.inc.php";
         ?>
+        <img src="img/citrouilleascii.png" alt="Image de profil" id="profilePicture"/>
+        <h1>Bienvenue !</h1>
     </body>
 </html>
